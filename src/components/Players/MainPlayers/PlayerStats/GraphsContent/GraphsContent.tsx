6a7aa7c4ae75ec772d681a85dc968fe85bdfbfa5
BoubaWade <117430608+BoubaWade@users.noreply.github.com>
2024-01-28 22:39:47 +0100

    ✨ F2 > PLAYERS > creation Line graph for specific stat

--- src/components/Players/MainPlayers/PlayerStats/GraphsContent/GraphsContent.tsx
+++ src/components/Players/MainPlayers/PlayerStats/GraphsContent/GraphsContent.tsx
@@ -1,23 +1,24 @@
+import "./graphsContent.css";
 import { MenuSelect } from "react-menu-dropdown-list";
 import { seasons } from "../../../../../config/constants";
 import { getPlayerStatsBySeason } from "../../../../../service/apiCall";
 import { useContext, useState } from "react";
 import { PlayersContext } from "../../../../../contexts/playersContext";
-import LinePlot from "./GraphPoints";
-import { PlayerStats } from "../../../playersTypes";
 import { getSpecificStat } from "../../../../../helpers/players";
-// import { PlayerStats } from "../../../playersTypes";
+import Line from "./Line";
 
 export default function GraphsContent() {
   const { playerStats, playerStatsBySeason, setPlayerStatsBySeason } =
     useContext(PlayersContext);
   const [specificStat, setSpecificStat] = useState<number[]>([]);
-  //   const [playerStatsBySeason, setPlayerStatsBySeason] = useState<PlayerStats[]>(
-  //     []
-  //   );
+  const [statName, setStatName] = useState("");
+  // const [playerStatsBySeason, setPlayerStatsBySeason] = useState<PlayerStats[]>(
+  //   []
+  // );
   const handleSelectStatName = (option: string) => {
     const playerId = playerStats[0]?.player.id;
     getPlayerStatsBySeason(playerId, parseInt(option), setPlayerStatsBySeason);
+    setStatName(option);
   };
   const handleSelectSeason = (option: string) => {
     const specificStat = getSpecificStat(playerStatsBySeason, option);
@@ -25,38 +26,49 @@ export default function GraphsContent() {
   };
 
   return (
-    <div>
-      <div>
-        <label>Statistiques du joueur</label>
-        <MenuSelect
-          options={["Points", "Passes", "Rebonds"]}
-          onSelect={(option) => handleSelectStatName(option)}
-          classNameContainer=""
-          classNameButton=""
-          classNameList=""
-          classNameItem=""
+    <div className="graphs-content">
+      <div className="menu-select-container">
+        <div className="stats">
+          <label>Statistiques du joueur</label>
+          <MenuSelect
+            options={["Points", "Passes", "Rebonds"]}
+            onSelect={(option) => handleSelectStatName(option)}
+            classNameContainer=""
+            classNameButton=""
+            classNameList=""
+            classNameItem=""
+          />
+        </div>
+        <div className="season">
+          <label>Saison</label>
+          <MenuSelect
+            options={seasons}
+            onSelect={(option) => handleSelectSeason(option)}
+            classNameContainer=""
+            classNameButton=""
+            classNameList=""
+            classNameItem=""
+          />
+        </div>
+      </div>
+      <div className="line-graph-container">
+        <Line
+          data={[]}
+          statName={statName}
+          className="line-graph"
+          classNameStat="stat-name"
+          width={500}
+          height={300}
+          marginTop={20}
+          marginRight={20}
+          marginBottom={20}
+          marginLeft={20}
         />
       </div>
-      <div>
-        <label>Saison</label>
-        <MenuSelect
-          options={seasons}
-          onSelect={(option) => handleSelectSeason(option)}
-          classNameContainer=""
-          classNameButton=""
-          classNameList=""
-          classNameItem=""
-        />
-      </div>
-      <LinePlot
-        data={specificStat}
-        width={640}
-        height={400}
-        marginTop={20}
-        marginRight={20}
-        marginBottom={20}
-        marginLeft={20}
-      />
+      {/* <Bar
+        data={[1.2, 5, 1.3, 1.8, 2.2, 3, 1.8, 4, 1.7]}
+        dimensions={dimensions}
+      /> */}
     </div>
   );
 }
---
+++ src/components/Players/MainPlayers/PlayerStats/GraphsContent/Line.tsx
@@ -0,0 +1,71 @@
+import * as d3 from "d3";
+import { useRef, useEffect } from "react";
+type LineProps = {
+  data: number[];
+  statName: string;
+  className: string;
+  classNameStat: string;
+  width: number;
+  height: number;
+  marginTop: number;
+  marginRight: number;
+  marginBottom: number;
+  marginLeft: number;
+};
+
+export default function Line({
+  data,
+  statName,
+  className,
+  classNameStat,
+  width,
+  height,
+  marginTop,
+  marginRight,
+  marginBottom,
+  marginLeft,
+}: LineProps) {
+  const gx = useRef<SVGGElement | null>(null);
+  const gy = useRef<SVGGElement | null>(null);
+  const x = d3
+    .scaleLinear()
+    .domain([0, data.length - 1])
+    .range([marginLeft, width - marginRight]);
+
+  const y = d3
+    .scaleLinear()
+    .domain(d3.extent(data) as [number, number])
+    .range([height - marginBottom, marginTop]);
+  const line = d3.line((_, i) => x(i), y);
+  useEffect(() => {
+    if (gx.current) {
+      d3.select(gx.current).call(d3.axisBottom(x));
+    }
+  }, [gx, x]);
+  useEffect(() => {
+    if (gy.current) {
+      d3.select(gy.current).call(d3.axisLeft(y));
+    }
+  }, [gy, y]);
+
+  return (
+    <div className={className}>
+      <svg width={width} height={height}>
+        <g ref={gx} transform={`translate(0,${height - marginBottom})`} />
+        <g ref={gy} transform={`translate(${marginLeft},0)`} />
+        <path
+          fill="none"
+          stroke="black"
+          strokeWidth="1.5"
+          d={line(data) || undefined}
+        />
+        <g fill="white" stroke="currentColor" strokeWidth="1.5">
+          {data.map((d, i) => (
+            <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />
+          ))}
+        </g>
+      </svg>
+      <span className={classNameStat}>{statName}</span>
+    </div>
+  );
+}
