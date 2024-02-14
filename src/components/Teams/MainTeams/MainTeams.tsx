13ee99d0d3bb6980fa87302cbd6d4b63045b2a97
BoubaWade <117430608+BoubaWade@users.noreply.github.com>
2024-02-14 22:10:07 +0100

    🚧 P4 > TEAMS > MainTeams > Create animation on hovered Card Division

--- src/components/Teams/MainTeams/MainTeams.tsx
+++ src/components/Teams/MainTeams/MainTeams.tsx
@@ -14,6 +14,7 @@ export default function MainTeams() {
 
   return (
     <div className="main-teams">
+      <h2>Liste des équipes par division</h2>
       <div className="divisions-container">
         {divisionsWithTeams.map((division, index) => (
           <DivisionTeam key={index} division={division} />
