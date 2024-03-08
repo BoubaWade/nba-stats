a5a2f996b81d5d006b860fa26f8badc92904743e
BoubaWade <117430608+BoubaWade@users.noreply.github.com>
2024-03-08 23:31:24 +0100

    ✨  F6 > TEAMS > SpecificTeamStats > Create Statistics layout

--- src/components/Teams/SpecificTeam/SpecificTeamStats/MainTeamStats/Statistics/Statistics.tsx
+++ src/components/Teams/SpecificTeam/SpecificTeamStats/MainTeamStats/Statistics/Statistics.tsx
@@ -1,3 +1,13 @@
+import "./statistics.css";
+
 export default function Statistics() {
-  return <div>Statistics</div>;
+  return (
+    <div className="statistics-container">
+      <div className="graphs">
+        <div className="circle-graph-container">CirleGraph</div>
+        <div className="line-graph-container">LineGraph</div>
+      </div>
+      <div className="statistics-table-container">Table</div>
+    </div>
+  );
 }
