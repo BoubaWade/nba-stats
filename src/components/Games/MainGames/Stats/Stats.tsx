5e0b25d4ca48014edbe983f15481187aa11889d8
BoubaWade <117430608+BoubaWade@users.noreply.github.com>
2024-03-10 23:31:41 +0100

    🚧   F6 > GAMES > MainGames > Stats.tsx > Create players table stats

--- src/components/Games/MainGames/Stats/Stats.tsx
+++ src/components/Games/MainGames/Stats/Stats.tsx
@@ -3,24 +3,67 @@ import StatsCardsList from "./StatsCardsList";
 import { useContext } from "react";
 import { GlobalContext } from "../../../../contexts/globalContext";
 import SecondaryButton from "../../../reusable-ui/SecondaryButton/SecondaryButton";
+import { getStatIfTheyPlay } from "../../../../helpers/players";
+import { getStatslabelsAndValues } from "../../../../config/config";
+import TableHeader from "../../../reusable-ui/TableHeader";
 
 export default function Stats() {
-  const { setShowSpecificTeam } = useContext(GlobalContext);
+  const { gameStats, setShowSpecificTeam } = useContext(GlobalContext);
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleReturnToTeamGames = () => {
     setShowSpecificTeam(true);
     navigate("/teams");
   };
-
+  const handleReturnToAllGames = () => {
+    // setShowSpecificTeam(true);
+    // navigate("/teams");
+  };
+  const statsToDisplay = getStatIfTheyPlay(gameStats);
+  const tableHeaderFields = [
+    "Points",
+    "Rebonds",
+    "Passes",
+    "Contre",
+    "Interceptions",
+    "Perte de balles",
+  ];
   return (
     <>
       <SecondaryButton
         className="return-button"
         label="Retour"
-        onClick={handleClick}
+        onClick={handleReturnToTeamGames}
       />
       <StatsCardsList />
+      <div className="players-tables-stats">
+        <SecondaryButton
+          className="return-button"
+          label="Retour"
+          onClick={handleReturnToAllGames}
+        />
+        <div className="table-container">
+          <table>
+            <TableHeader datas={tableHeaderFields} />
+            <tbody>
+              {statsToDisplay.map((stats) => (
+                <div>
+                  <h3>
+                    {stats.player.first_name} {stats.player.last_name}
+                  </h3>
+                  {getStatslabelsAndValues(stats).map(({ label, value }) => (
+                    <div>
+                      <p key={label}>
+                        {label} : <span>{value}</span>
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
